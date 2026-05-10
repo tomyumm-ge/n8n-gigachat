8fbccf6b7492be702c382ffa2110d82d2d13c84c
tomyumm-ge <msmyk1999@gmail.com>
2026-05-10 03:59:35 +0300

    Update disclaimer

--- nodes/shared/Disclaimers.ts
+++ nodes/shared/Disclaimers.ts
@@ -3,7 +3,7 @@ import { INodeProperties } from 'n8n-workflow';
 const disclaimerBlocks: INodeProperties[] = [
 	{
 		displayName:
-			'<b>Неофициальный проект</b><br/>На ваш страх и риск. Это не сберовский узел.<br/><a href="https://github.com/tomyumm-ge/n8n-gigachat/wiki/%E2%9A%A0%EF%B8%8F-Disclaimers" target="_blank">Узнать подробнее</a>',
+			'Неофициальный узел. Прочтите <a href="https://github.com/tomyumm-ge/n8n-gigachat/wiki/%E2%9A%A0%EF%B8%8F-Disclaimers" target="_blank">дисклеймер</a>.',
 		name: 'unofficialWarning',
 		type: 'notice',
 		default: '',
